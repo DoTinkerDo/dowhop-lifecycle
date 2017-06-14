1b058c05351a9bcb53871a10d376343cf8ca1c9e
omarchaeopteryx <omaralimalik@gmail.com>
2017-06-13 23:23:03 -0700

    Move legacy maps code, continued

--- scripts/coordinate.js
+++ scripts/coordinate.js
@@ -1,73 +1,6 @@
 // Use code to coordinate DoWhops.
 
 var currentSessionID;
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -33.8688, lng: 151.2195 },
-    zoom: 13,
-    mapTypeId: 'roadmap'
-  });
-
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log('Returned place contains no geometry');
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        })
-      );
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
 
 function getSesh(node) {
   FriendlyChat.prototype.getSession();
@@ -121,7 +54,6 @@ function FriendlyChat() {
   this.pendingDiv = document.getElementById('pending-div');
   this.submitApproval = document.getElementById('submit-approval-button');
   this.submitRescind = document.getElementById('submit-rescind-button');
-  this.chatInputMap = document.getElementById('map');
 
   // Load chat data:
   this.chatItemData = document.getElementById('coordinate-tab');
