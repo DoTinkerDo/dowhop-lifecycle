bed03af30fd2d1e91b8e4829c35f56e7c89e24c2
omarchaeopteryx <omaralimalik@gmail.com>
2017-06-15 16:19:07 -0700

    Register current user in profile form submit

--- scripts/dowhop-profile-form-create.js
+++ scripts/dowhop-profile-form-create.js
@@ -1,10 +1,44 @@
 // This is the script for dealing with users' profiles.
 
 //  Section for creating profile for first-time users:
+
+var createProfileForm = document.getElementById('create-profile-form');
+var createProfileName = document.getElementById('profile-name');
+var createProfileAbout = document.getElementById('profile-about');
+var createProfileActivity1 = document.getElementById('profile-activity-1');
+var createProfileFormBtn = document.getElementById('create-profile-form-button');
+
+createProfileFormBtn.addEventListener('click', createProfile);
+
 function addDoWhopImage(files_arr, node) {
+  console.log("You've uploaded a file...");
+  console.log(files_arr[0]);
   return (file = files_arr[0]);
   if (!file.type.match('image/.*')) {
     alert('You can only add images at the moment.');
     return;
   }
+}
+
+function createProfile(e) {
+    e.preventDefault();
+    console.log("You submitted a new profile!");
+    var currentProfile = firebase.auth().currentUser.uid;
+    // var profileRef = firebase.database().ref('app_users/' + currentProfile);
+    console.log('creating profile');
+    console.log(currentProfile);
+    createProfileForm.reset();
+  // });
+
+  // var profileData = {
+  //   profileName: createProfileName.value,
+  //   profileAbout: createProfileName.value,
+  //   createProfileActivity1: createProfileActivity1.value
+  // }
+  //
+  // profileRef.update(profileData).then(function() {
+  //   consoel.log("Successful!");
+  // });
+  console.log("resetting form!");
+  // createProfileForm.reset();
 }
