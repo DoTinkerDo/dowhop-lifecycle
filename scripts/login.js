4f1b4416064f4bb54adac5d51bd2eb4a4de96310
omarchaeopteryx <omaralimalik@gmail.com>
2017-06-21 18:21:11 -0700

    Refactor logic

--- scripts/login.js
+++ scripts/login.js
@@ -35,6 +35,7 @@
       };
       appUserRef.set(userData);
     });
+    checkForDefaultDoWhop();
   }
 
   function handleSignedInUser(user) {
@@ -90,3 +91,41 @@ auth.onAuthStateChanged(function(user) {
     console.log('PERSON signed out');
   }
 });
+
+function createDefaultDoWhop() {
+  var uid = auth.currentUser.uid;
+  var email = auth.currentUser.email;
+  var creatorDescription = auth.currentUser.email;
+  var doWhopDescriptionKey = doWhopDescriptionRef.push().key;
+  // var filepath;
+  var defaultImageURL = 'https://static.wixstatic.com/media/de271e_a0f92b126d584e54a84a2f721c1571d4~mv2_d_3543_2480_s_4_2.jpg/v1/crop/x_0,y_221,w_3543,h_1159/fill/w_886,h_246,al_c,q_80,usm_0.66_1.00_0.01/de271e_a0f92b126d584e54a84a2f721c1571d4~mv2_d_3543_2480_s_4_2.webp';
+
+
+  // Then we add on note to user's profile that it has been added:
+  var appUsersRef = database.ref('/app_users');
+  var appUserRef = appUsersRef.child(user.uid);
+  appUserRef.once('value').then(function(snapshot) {
+    if (snapshot.val().hasDefaultDoWhop === true) return;
+
+    // First we create the new default DoWhop:
+    doWhopDescriptionRef.child(doWhopDescriptionKey).set({
+        createdBy: uid,
+        doWhopDescriptionKey: doWhopDescriptionKey,
+        downloadURL: defaultImageURL,
+        titleDescription: "DoWhop with us!",
+        whoDescription: "DoWhop Team is here to help you!",
+        whatDescription: "",
+        whenDescription: "",
+        whereDescription: "",
+        howMuchDescription: "",
+        creatorDescription: "tinkerdowhop@gmail.com",
+        doerDescription: email
+      });
+    console.log('created default dowhop');
+
+    var userData = {
+          hasDefaultDoWhop: true
+        };
+    appUserRef.update(userData);
+  });
+}
