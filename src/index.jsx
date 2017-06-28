06dad4b7a7b61967798c3626a8a9a2427b73629a
JohannBillar <jwbillar@gmail.com>
2017-06-28 15:17:10 -0700

    Configure Babel with presets react, env target browsers last 2 versions, loose true, modules false

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './Application';
+import { render } from 'react-dom';
+import Application from './components/Application';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-
-ReactDOM.render(<Application />, document.getElementById('review'));
+render(<Application />, document.getElementById('root'));
