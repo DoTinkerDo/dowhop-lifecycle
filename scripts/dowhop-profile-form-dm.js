a2d22a1228b83e3820a76bb34ec46a04cdf0ea70
omarchaeopteryx <omaralimalik@gmail.com>
2017-06-28 19:45:39 -0700

    Add function for getting user A and user B for chat

--- scripts/dowhop-profile-form-dm.js
+++ scripts/dowhop-profile-form-dm.js
@@ -3,9 +3,23 @@
 // Gathering our DOM elements.
 var sendDMButton = document.getElementById('send-direct-message');
 
-sendDMButton.addEventListener('click', initiateChat());
+sendDMButton.addEventListener('click', initiateChat);
+
+function getPersonOne() {
+  let user1 = firebase.auth().currentUser.uid;
+  return user1
+}
+
+function getPersonTwo() {
+  let user2 = sendDMButton.parentNode.id;
+  return user2
+}
 
 function initiateChat(e) {
   e.preventDefault();
-  console.log("Hello! You have started a chat!");
+  console.log("You have started a chat!", getPersonOne());
+  console.log("You will be chatting with:", getPersonTwo());
+  // var personOne = user;
+
+
 }
