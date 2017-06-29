e02ef5a56427c1bd8a883024713796c975d3cc28
omarchaeopteryx <omaralimalik@gmail.com>
2017-06-28 20:25:30 -0700

    Create FB path reference

--- scripts/dowhop-profile-form-dm.js
+++ scripts/dowhop-profile-form-dm.js
@@ -3,7 +3,7 @@
 // Gathering our DOM elements.
 var sendDMButton = document.getElementById('send-direct-message');
 
-sendDMButton.addEventListener('click', initiateChat);
+sendDMButton.addEventListener('click', initiateChatDM);
 
 function getPersonOne() {
   let user1 = firebase.auth().currentUser.uid;
@@ -15,11 +15,18 @@ function getPersonTwo() {
   return user2
 }
 
-function initiateChat(e) {
+// This will ensure the viewer and recipient are pointed toward the same root reference:
+function createPathDM(user1, user2) {
+  return 'chat_'+(user1<user2 ? user1+'_'+user2 : user2+'_'+user1);
+}
+
+function initiateChatDM(e) {
   e.preventDefault();
   console.log("You have started a chat!", getPersonOne());
   console.log("You will be chatting with:", getPersonTwo());
-  // var personOne = user;
-
+  let refTail = createPathDM(getPersonOne(), getPersonTwo());
+  let chatDMref = firebase.database().ref('/direct-messages').child(refTail);
+  console.log(refTail);
+  chatDMref.push({test: "hi"});
 
 }
