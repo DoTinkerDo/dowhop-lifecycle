c3fa9850772611695a798c76930e4ce25441a5e4
omarchaeopteryx <omaralimalik@gmail.com>
2017-06-29 18:08:13 -0700

    Add section for displaying DM thread history; successfully show recent message to user

--- scripts/dowhop-profile-form-dm.js
+++ scripts/dowhop-profile-form-dm.js
@@ -1,63 +1,94 @@
-'use strict'
+// This is a script for managing direct-messaging functionality.
+// (c) DoWhop.com, 2017.
+'use strict';
 
 // Gathering our DOM elements.
 var sendDMButton = document.getElementById('send-direct-message');
 sendDMButton.addEventListener('click', revealFormDM);
 
 function getPersonOne() {
-  let user1 = firebase.auth().currentUser.uid;
-  return user1
+	let user1 = firebase.auth().currentUser.uid;
+	return user1;
 }
 
 function getPersonTwo() {
-  let user2 = sendDMButton.parentNode.id;
-  return user2
+	let user2 = sendDMButton.parentNode.id;
+	return user2;
 }
 
 // This will ensure the viewer and recipient are pointed toward the same root reference:
 function createPathDM(user1, user2) {
-  return 'chat_'+(user1<user2 ? user1+'_'+user2 : user2+'_'+user1);
+	return 'chat_' + (user1 < user2 ? user1 + '_' + user2 : user2 + '_' + user1);
 }
 
 function initiateChatDM() {
-  // var directMessageButton =  document.getElementById('direct-message-form-button');
-  // directMessageButton.addEventListener('click', sendDirectMessage());
-  //
-  // var directMessageButtonHide =  document.getElementById('direct-message-form-button-hide');
-  // directMessageButtonHide.addEventListener('click', hideFormDM());
-  //
-  // console.log("You have started a chat!", getPersonOne());
-  // console.log("You will be chatting with:", getPersonTwo());
+	// var directMessageButton =  document.getElementById('direct-message-form-button');
+	// directMessageButton.addEventListener('click', sendDirectMessage());
+	//
+	// var directMessageButtonHide =  document.getElementById('direct-message-form-button-hide');
+	// directMessageButtonHide.addEventListener('click', hideFormDM());
+	//
+	// console.log("You have started a chat!", getPersonOne());
+	// console.log("You will be chatting with:", getPersonTwo());
 }
 
 function sendDirectMessage(e) {
-  e.preventDefault();
-  var message = document.getElementById('direct-message-form-input');
-  let sender = getPersonOne();
-  let recipient = getPersonTwo();
-  let refTail = createPathDM(getPersonOne(), getPersonTwo());
-  let chatDMref = firebase.database().ref('/direct-messages').child(refTail);
-  console.log(refTail);
-  chatDMref.push({
-    from: sender,
-    to: recipient,
-    body: message.value})
-      .then(document.getElementById('direct-message-form')
-      .reset());
+	e.preventDefault();
+	var message = document.getElementById('direct-message-form-input');
+	let sender = getPersonOne();
+	let recipient = getPersonTwo();
+	let refTail = createPathDM(getPersonOne(), getPersonTwo());
+	let chatDMref = firebase.database().ref('/direct-messages').child(refTail);
+	console.log(refTail);
+	chatDMref
+		.push({
+			from: sender,
+			to: recipient,
+			body: message.value
+		})
+		.then(document.getElementById('direct-message-form').reset());
 }
 
 function revealFormDM(e) {
-  e.preventDefault();
-  document.getElementById('direct-message-form-button').addEventListener('click', sendDirectMessage);
-  document.getElementById('direct-message-form-button-hide').addEventListener('click', hideFormDM);
-  var directMessageForm = document.getElementById('direct-message-form');
-  directMessageForm.removeAttribute('hidden');
-  console.log("You have started a chat!", getPersonOne());
-  console.log("You will be chatting with:", getPersonTwo());
+	e.preventDefault();
+	document.getElementById('direct-message-form-button').addEventListener('click', sendDirectMessage);
+	document.getElementById('direct-message-form-button-hide').addEventListener('click', hideFormDM);
+	var directMessageForm = document.getElementById('direct-message-form');
+	directMessageForm.removeAttribute('hidden');
+	console.log('You have started a chat!', getPersonOne());
+	console.log('You will be chatting with:', getPersonTwo());
+	loadDirectMessageHistory();
 }
 
 function hideFormDM(e) {
-  e.preventDefault();
-  var directMessageForm = document.getElementById('direct-message-form');
-  directMessageForm.setAttribute('hidden', 'true');
+	e.preventDefault();
+	var directMessageForm = document.getElementById('direct-message-form');
+	directMessageForm.setAttribute('hidden', 'true');
+}
+
+function loadDirectMessageHistory() {
+	var directMessageDiv = document.getElementById('direct-messages-div');
+	let refTail = createPathDM(getPersonOne(), getPersonTwo());
+	let chatDMref = firebase.database().ref('/direct-messages').child(refTail);
+
+	// Reset elements.
+	chatDMref.off();
+	directMessageDiv.innerText = '';
+
+	// Loads the last x number of messages and listen for new ones:
+	var setMessage = function(data) {
+		var val = data.val();
+		displayMessage(data.key, val.from, val.to, val.body);
+	}.bind(this);
+
+	chatDMref.orderByKey().limitToLast(12).on('child_added', setMessage);
+	chatDMref.orderByKey().limitToLast(12).on('child_changed', setMessage);
+}
+
+function displayMessage(key, from, to, body) {
+	var directMessageDiv = document.getElementById('direct-messages-div');
+	console.log(body);
+	var div = document.createElement('div');
+	div.innerText = body;
+	directMessageDiv.append(div);
 }
