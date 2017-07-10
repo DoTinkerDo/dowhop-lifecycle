391ddbfb91d01a867ae67ff6ca914d45c30fd282
omarchaeopteryx <omaralimalik@gmail.com>
2017-07-10 15:27:53 -0700

    Restore image load, render functionality in coordainte chat

--- scripts/coordinate.js
+++ scripts/coordinate.js
@@ -551,8 +551,9 @@ FriendlyChat.prototype.setImageUrl = function(imageUri, imgElement) {
   // If the image is a Cloud Storage URI we fetch the URL.
   if (imageUri.startsWith('gs://')) {
     imgElement.src = FriendlyChat.LOADING_IMAGE_URL; // Display a loading image first.
-    this.storage.refFromURL(imageUri).getMetadata().then(function(metadata) {
-      imgElement.src = metadata.downloadURL[0];
+    firebase.storage().refFromURL(imageUri).getMetadata().then(function(metadata) {
+      console.log(metadata);
+      imgElement.src = metadata.downloadURLs[0];
     });
   } else {
     imgElement.src = imageUri;
@@ -563,47 +564,54 @@ FriendlyChat.prototype.setImageUrl = function(imageUri, imgElement) {
 // This first saves the image in Firebase storage.
 FriendlyChat.prototype.saveImageMessage = function(event) {
   event.preventDefault();
+  console.log('files...', event.target.files);
+
   var file = event.target.files[0];
+
+  console.log('file:', file);
 
   // Clear the selection in the file picker input.
   this.imageForm.reset();
 
   // Check if the file is an image.
   if (!file.type.match('image.*')) {
-    var data = {
-      message: 'You can only share images',
-      timeout: 2000
-    };
-    this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
+    window.alert('You can only share images.');
+    // var data = {
+    //   message: 'You can only share images',
+    //   timeout: 2000
+    // };
+    // this.signInSnackbar.MaterialSnackbar.showSnackbar(data);
     return;
   }
   // Check if the user is signed-in
-  if (this.checkSignedInWithMessage()) {
-    // We add a message with a loading icon that will get updated with the shared image.
-    var currentUser = person;
-    this.messagesRef
-      .push({
-        name: currentUser.displayName,
-        imageUrl: FriendlyChat.LOADING_IMAGE_URL,
-        photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
-      })
-      .then(
-        function(data) {
-          // Upload the image to Cloud Storage.
-          var filePath = currentUser.uid + '/' + data.key + '/' + file.name;
-          return this.storage.ref(filePath).put(file).then(
-            function(snapshot) {
-              // Get the file's Storage URI and update the chat message placeholder.
-              var fullPath = snapshot.metadata.fullPath;
-              return data.update({ imageUrl: this.storage.ref(fullPath).toString() });
-            }.bind(this)
-          );
-        }.bind(this)
-      )
-      .catch(function(error) {
-        console.error('There was an error uploading a file to Cloud Storage:', error);
-      });
-  }
+  // if (this.checkSignedInWithMessage()) {
+  // We add a message with a loading icon that will get updated with the shared image.
+  var currentUser = person;
+  this.messagesRef
+    .push({
+      name: currentUser.displayName,
+      imageUrl: FriendlyChat.LOADING_IMAGE_URL,
+      photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
+    })
+    .then(
+      function(data) {
+        // Upload the image to Cloud Storage.
+        var filePath = 'userImages/' + currentUser.uid + '/' + 'messageImages/' + data.key + '/' + file.name;
+        return this.storage.ref(filePath).put(file).then(
+          function(snapshot) {
+            // Get the file's Storage URI and update the chat message placeholder.
+            console.log('filePath:', filePath);
+            var fullPath = snapshot.metadata.fullPath;
+            console.log('fullPath:', fullPath);
+            return data.update({ imageUrl: this.storage.ref(fullPath).toString() });
+          }.bind(this)
+        );
+      }.bind(this)
+    )
+    .catch(function(error) {
+      console.error('There was an error uploading a file to Cloud Storage:', error);
+    });
+  // }
 };
 
 // Signs-in Friendly Chat.
