23c053b9b61d13de953a1c50016da841619c543e
omarcodex <omaralimalik@gmail.com>
2017-09-06 14:23:25 -0700

    Re-add function profileProgessNodeInit lost in merge action

--- scripts/login.js
+++ scripts/login.js
@@ -22,6 +22,25 @@
   var loginPage = document.getElementById('login-container');
   var applicationPage = document.getElementById('application-container');
 
+  function profileProgessNodeInit(user) {
+ -    //This function will check if this node exists and create it if it doesn't
+ -    //and not overwrite it if it does
+ -    var currentUserRef = database.ref('/app_users').child(user.uid);
+ -    var profileProgressDataInit;
+ -    profileProgressDataInit = {
+ -      profileProgress: {
+ -        'verify-phone': false,
+ -        'verify-email': false,
+ -        'verify-social': false
+ -      }
+ -    };
+ -    currentUserRef.once('value').then(function(snapshot) {
+ -      if (snapshot.val().profileProgress === undefined) {
+ -        currentUserRef.update(profileProgressDataInit);
+ -      }
+ -    });
+ -  }
+
   function writeUserData(user) {
     var appUsersRef = database.ref('/app_users');
     var appUserRef = appUsersRef.child(user.uid);
@@ -41,6 +60,7 @@
   }
 
   function handleSignedInUser(user) {
+    profileProgessNodeInit(user);
     loginPage.style.display = 'none';
     applicationPage.style.display = 'block';
     writeUserData(user);
