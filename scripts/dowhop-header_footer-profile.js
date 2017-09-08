a5a64c4ced8bd080a82071ab587da53145cae579
shar-ajmal <sherio777@gmail.com>
2017-09-08 11:52:25 -0700

    Added the header to the DoWhop Profile Page

--- scripts/dowhop-header_footer-profile.js
+++ scripts/dowhop-header_footer-profile.js
@@ -1,20 +1,35 @@
-myProfilePicture = document.getElementById('my-profile-picture');
+'use strict'
 
-function retrieveProfile() {
-	var currentUser = auth.currentUser.uid;
+var myProfilePicture = document.getElementById('my-profile-picture-header');
+//myProfilePicture.src="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg";
+console.log("In header file");
+var url = "";
 
+function getProfilePic(user) {
+	var currentUser = user.uid
+	console.log("hello");
 	var profileRef = database.ref('app_users/' + currentUser);
 
-	profileRef.once('value', function(snap) {
-		appUser = snap.val();
-		myProfilePicture.src = appUser.photoURL;
+	profileRef.on('value', function(snap) {
+		var appUser = snap.val();
+		//myProfilePicture.src="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg";
+
+		return appUser.photoURL;
+		//console.log(myProfilePicture.src);
 	});
+	myProfilePicture.src="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg";
 }
 
-// For looking at your own profile (user is logged in):
 auth.onAuthStateChanged(function(user) {
-	console.log(user);
-  if (user) {
-    retrieveProfile();
-  }
-});
+	if(user) {
+		url = getProfilePic(user);
+		console.log("User is signed in");
+		console.log(url);	
+
+		//console.log(url);
+		//myProfilePicture.src = url;
+	}
+	else {
+		console.log("Not Signed In");
+	}
+})
