ae5f81a445e550a3333ec06d506d1a7216ca75c8
omarcodex <omaralimalik@gmail.com>
2017-10-04 10:53:13 -0700

    Undo firebase db switch

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,19 +1,11 @@
 // DoWhop Lifecycle DB:
 var config = {
-  // apiKey: 'AIzaSyB95x1zEsSkXfaDgOVdTw7ESavk9O9geN0',
-  // authDomain: 'dowhop-lifecycle.firebaseapp.com',
-  // databaseURL: 'https://dowhop-lifecycle.firebaseio.com',
-  // projectId: 'dowhop-lifecycle',
-  // storageBucket: 'dowhop-lifecycle.appspot.com',
-  // messagingSenderId: '1090371045772'
-
-  // Omar's
-  apiKey: 'AIzaSyCMOlta_VjzsPj96ad1wN-u1kQyz0wQuI8',
-  authDomain: 'dowhop-lifecycle-admin.firebaseapp.com',
-  databaseURL: 'https://dowhop-lifecycle-admin.firebaseio.com',
-  projectId: 'dowhop-lifecycle-admin',
-  storageBucket: 'dowhop-lifecycle-admin.appspot.com',
-  messagingSenderId: '1012883670556'
+  apiKey: 'AIzaSyB95x1zEsSkXfaDgOVdTw7ESavk9O9geN0',
+  authDomain: 'dowhop-lifecycle.firebaseapp.com',
+  databaseURL: 'https://dowhop-lifecycle.firebaseio.com',
+  projectId: 'dowhop-lifecycle',
+  storageBucket: 'dowhop-lifecycle.appspot.com',
+  messagingSenderId: '1090371045772'
 };
 
 firebase.initializeApp(config);
