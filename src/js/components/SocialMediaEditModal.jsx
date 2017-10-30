3e122723a56e752566d009d3842613a97fe08bcf
JohannBillar <jwbillar@gmail.com>
2017-10-29 19:52:05 -0700

    Make input fields controlled components

--- src/js/components/SocialMediaEditModal.jsx
+++ src/js/components/SocialMediaEditModal.jsx
@@ -1,20 +1,35 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input, CardSubtitle, CardText, Modal, ModalBody, ModalFooter } from 'reactstrap';
+import { Form, FormGroup, Label, Input, CardSubtitle, ModalHeader, Modal, ModalBody, ModalFooter } from 'reactstrap';
 import DoWhopButton from './DoWhopButton';
 
 type Props = {};
 type State = {
-  modal: boolean
+  modal: boolean,
+  valueFB: string,
+  valueTW: string,
+  valueIG: string,
+  valueIN: string
 };
 
 class SocialMediaEditModal extends Component<Props, State> {
   state = {
-    modal: false
+    modal: false,
+    valueFB: '',
+    valueTW: '',
+    valueIG: '',
+    valueIN: ''
   };
 
   toggle = () => this.setState(prevState => ({ modal: !prevState.modal }));
+
+  handleChange = (e, site) => {
+    const URL = e.target.value;
+    this.setState({
+      [site]: URL
+    });
+  };
 
   render() {
     return (
@@ -23,42 +38,69 @@ class SocialMediaEditModal extends Component<Props, State> {
           Social Media
           <span className="fa fa-pencil pencil-social" onClick={this.toggle} aria-hidden="true" />
         </CardSubtitle>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className="">
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalBody>
-            <CardText toggle={this.toggle}>Enter the URLs to your social media profiles below</CardText>
-
-            <Form>
+            <ModalHeader toggle={this.toggle}>Enter the URLs to your social media profiles below</ModalHeader>
+            <Form className="social-media-update-form">
               <FormGroup>
                 <Label for="facebook">
                   Facebook URL
-                  <Input type="text" name="social-facebook" id="facebook" />
-                  <br />
+                  <Input
+                    type="text"
+                    size="sm"
+                    value={this.state.valueFB}
+                    name="social-facebook"
+                    id="facebook"
+                    placeholder="facebook.com/your-name"
+                    onChange={e => this.handleChange(e, 'valueFB')}
+                  />
                 </Label>
+                <br />
                 <Label for="twitter">
                   Twitter URL
-                  <Input type="text" name="social-twitter" id="twitter" />
-                  <br />
+                  <Input
+                    type="text"
+                    size="sm"
+                    value={this.state.valueTW}
+                    name="social-twitter"
+                    id="twitter"
+                    placeholder="twitter.com/your-handle"
+                    onChange={e => this.handleChange(e, 'valueTW')}
+                  />
                 </Label>
+                <br />
                 <Label for="instagram">
                   Instagram URL
-                  <Input type="text" name="social-instagram" id="instagram" />
-                  <br />
+                  <Input
+                    type="text"
+                    size="sm"
+                    value={this.state.valueIG}
+                    name="social-instagram"
+                    id="instagram"
+                    placeholder="instagram.com/your-handle"
+                    onChange={e => this.handleChange(e, 'valueIG')}
+                  />
                 </Label>
+                <br />
                 <Label for="exampleSelect">
-                  Linked URL
-                  <Input type="text" name="social-linkedin" id="linkedin" />
-                  <br />
+                  LinkedIN URL
+                  <Input
+                    type="text"
+                    size="sm"
+                    value={this.state.valueIN}
+                    name="social-linkedin"
+                    id="linkedin"
+                    placeholder="linkedin.com/in/your-profile"
+                    onChange={e => this.handleChange(e, 'valueIN')}
+                  />
                 </Label>
+                <br />
               </FormGroup>
             </Form>
           </ModalBody>
           <ModalFooter>
-            <DoWhopButton color="primary" onClick={this.toggle}>
-              Save
-            </DoWhopButton>
-            <DoWhopButton color="secondary" onClick={this.toggle}>
-              Cancel
-            </DoWhopButton>
+            <DoWhopButton onClick={this.toggle}>Save</DoWhopButton>
+            <DoWhopButton onClick={this.toggle}>Cancel</DoWhopButton>
           </ModalFooter>
         </Modal>
       </div>
