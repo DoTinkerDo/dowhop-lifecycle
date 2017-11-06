b75b2b17febe16518cf8ae907ac02cd66de07438
omarcodex <omaralimalik@gmail.com>
2017-11-06 12:16:06 -0800

    Add raw data viewer for given DoWhop ID

---
+++ dist/scripts/admin-portal-demo.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Function for generating raw view of data. Assumes user has DoWhop ID ready.
+
+function retrieveSingleDoWhop(e) {
+  e.preventDefault();
+  var searchTerm = document.getElementById("given-dowhop-id").value || "NA";
+  console.log(  firebase.auth().currentUser );
+  console.log(searchTerm);
+
+  var tempRef = database.ref('/DoWhopDescriptions').child(searchTerm);
+  tempRef.once('value').then((function(snap) {
+    searchResults.innerHTML = JSON.stringify(snap.val())
+  }))
+}
+
+var searchButton = document.getElementById('search-for-dowhop');
+searchButton.addEventListener('click', retrieveSingleDoWhop);
+var searchResults = document.getElementById('result');
